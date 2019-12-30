bbc9ca02e971b07a7d34805b2455eb2733455dc6
hoshinoyuri <453327983@qq.com>
2019-12-30 15:05:44 +0800

    yuri第一次提交

--- day48/src/com/yuri/demo1/Demo1.java
+++ day48/src/com/yuri/demo1/Demo1.java
@@ -3,6 +3,13 @@ package com.yuri.demo1;
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("aaa");
-		//pwd可以查看当前路径
+		//pwd可以查看当前路径　cd进入文件夹
+		//git init初始化
+		//git add 文件夹名称 提交到本地仓库 （表示把这个文件夹添加到git本地仓库里去）
+		/**
+		 * git commit -m "first commit" 追加提交的描述信息
+		 * git remote add origin https://github.com/hoshinoyuri666/demo01.git 建立本地仓库与远程仓库之间的关系
+		 * git push -u origin master 把本地代码提到远程仓库里
+		 */
 	}
 }
